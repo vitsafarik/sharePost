e6cde822c48f9384e56aead95ee621a447e5ebc6
Vit Safarik <vit@safarik.dev>
2020-11-01 12:45:41 +0100

    database setup

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
 
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "_YOUR_USER_");
-define("DB_PASS", "_YOUR_PASS_");
-define("DB_NAME", "_YOUR_DB_NAME_");
+define("DB_USER", "safarik");
+define("DB_PASS", "safarik");
+define("DB_NAME", "shareposts");
 
 // App Root
 define("APPROOT", dirname(dirname(__FILE__)));
 // URL Root
-define("URLROOT", "_YOUR_URL_");
+define("URLROOT", "http://localhost/sharePosts");
 // Site Name
-define("SITENAME", "_YOUR_SITE_NAME_");
+define("SITENAME", "SharePosts");
