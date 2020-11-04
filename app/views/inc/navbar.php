bf0e0730a94fc0e91fd853b0d97a1aa41bbef5e2
Vit Safarik <vit@safarik.dev>
2020-11-04 14:55:24 +0100

    user session and logout

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -14,16 +14,20 @@
                     <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
                 </li>
             </ul>
-
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
-                </li>
+                <?php if (isset($_SESSION["user_id"])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+                    </li>
+                <?php endif; ?>
             </ul>
-
         </div>
     </div>
 </nav>
