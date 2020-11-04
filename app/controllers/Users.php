edf1bede9a11d7ec22e76db6d26db0951abbd992
Vit Safarik <vit@safarik.dev>
2020-11-04 14:36:58 +0100

    user login

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -122,10 +122,26 @@ class Users extends Controller
                 $data["password_err"] = "Please enter password";
             }
 
+            // Check for user/email
+            if ($this->userModel->findUserByEmail($data["email"])) {
+                // User found
+            } else {
+                $data["email_err"] = "No user found";
+            }
+
             // Make sure errors are empty
             if (empty($data["email_err"]) && empty($data["password_err"])) {
                 // Validated
-                die("SUCCESS");
+                // Check and set logged in user
+                $loggedInUser = $this->userModel->login($data["email"], $data["password"]);
+
+                if ($loggedInUser) {
+                    // Create Session
+                    die("SUCCCESSS");
+                } else {
+                    $data["password_err"] = "Password incorrect";
+                    $this->view("users/login", $data);
+                }
             } else {
                 // Load view whit errors
                 $this->view("users/login", $data);
--- app/models/User.php
+++ app/models/User.php
@@ -24,8 +24,25 @@ class User
         $this->db->bind(':password', $data['password']);
 
         // Execute
-        if($this->db->execute()) {
+        if ($this->db->execute()) {
             return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Login User
+    public function login($email, $password)
+    {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(":email", $email);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+
+        if (password_verify($password, $hashed_password)) {
+            return $row;
         } else {
             return false;
         }
